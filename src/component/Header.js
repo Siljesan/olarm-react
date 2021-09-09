ab0224183905ccd78eb4f022d6d81e741f299884
Siljesandersen <silje_sandersen@hotmail.com>
2021-09-09 12:10:08 +0200

    created header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,41 +1,71 @@
-import React from 'react'
+import React from "react";
 
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Logo from '../img/olarm_logo.png'
-
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Logo from "../img/olarm_logo.png";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Header() {
-    return (
-<header className='bg-img'>
-<Navbar variant='dark' expand="lg">
-  <Container>
-  <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src={Logo}
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-      </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav" className='navLinks'>
-      <Nav className="me-auto">
-        <Nav.Link href="#home" className='active'>HOME</Nav.Link>
-        <Nav.Link href="#link" className='link'>FEATURES</Nav.Link>
-        <Nav.Link href="#link">PLANS</Nav.Link>
-        <Nav.Link href="#link">MEET OLARM</Nav.Link>
-      </Nav>
-      <Nav>
-      <Nav.Link className='active' href="">Sign up</Nav.Link>
-      <Nav.Link className='active navCta' href="">Sign in</Nav.Link>
-    </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-</header>
-    )
+  return (
+    <header className="bg-img">
+      <Navbar variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img alt="" src={Logo} className="d-inline-block align-top" />{" "}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="navLinks">
+            <Nav className="me-auto">
+              <Nav.Link href="#home" className="navLinks--active">
+                HOME
+              </Nav.Link>
+              <Nav.Link href="#link" className="link">
+                FEATURES
+              </Nav.Link>
+              <Nav.Link href="#link">PLANS</Nav.Link>
+              <Nav.Link href="#link">MEET OLARM</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link className="links" href="">
+                Sign up
+              </Nav.Link>
+              <Nav.Link className="links navCta" href="">
+                Sign in
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col className="textCont">
+            <h1 className="heading">
+              World's <span className="headingActive">Smartest</span> Home
+              Security
+            </h1>
+            <p className="supportText">
+              Unique security experiences with more than 300,000 clients, Simple
+              Installation, Convenience & Free Monitoring
+            </p>
+            <button className="ctaBtn">VISIT SHOP</button>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid>
+        <Row>
+          <Col>
+            <div className="icons">
+              <i className="fab fa-google"></i>
+              <i className="fab fa-twitter"></i>
+              <i className="fab fa-facebook-f"></i>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
